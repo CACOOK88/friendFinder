3c66ef45edeb2b0d312891a14344be1b3a2d849f
Chad Cook <chadcookcoding@gmail.com>
2019-01-07 06:47:09 -0500

    added logic to compare and return best friend results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,34 @@ module.exports = function(app) {
     res.json(friends)
   })
   app.post('/api/friends', function( req, res ) {
-    // NEED TO GRAB INFO FROM FORM TO ADD NEW FRIEND THEN 
-    // MAKE COMPARISON AND RESPOND TO BROWSER WITH THE
-    // CLOSEST FRIEND
+
+    let newFriend = req.body
+
+    friends.push(newFriend)
+ 
+    let newFriendTotal =  newFriend.scores.reduce(function(a, b) {
+        return parseInt(a) + parseInt(b)
+    }, 0);
+
+    let bestFriend = {}
+    let bestScore = 110000
+    for ( let i = 0; i < friends.length -1; i++ ) {
+      let friendScore = friends[i].scores.reduce(function(a, b) {
+                                                  return parseInt(a) + parseInt(b)
+                                                }, 0);
+      let score = 0
+
+      if ( friendScore >= newFriendTotal ) {
+        score = friendScore-newFriendTotal
+      } else {
+        score = newFriendTotal-friendScore
+      }
+
+      if ( score < bestScore ) {
+        bestFriend = friends[i] 
+      }
+    }
+
+    res.json(bestFriend)
   })
 }
